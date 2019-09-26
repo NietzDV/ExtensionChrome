614c4f7859d5ecdaf490ec9f40f337430da380ec
Yure Matias <ymatias2000@gmail.com>
2019-09-26 18:21:55 -0300

    Update content.js

--- Extension/content.js
+++ Extension/content.js
@@ -5,5 +5,5 @@ srcs.push('https://media.tenor.com/images/417a1e10300848c69abb9663124d3f68/tenor
 srcs.push('https://thumbs.gfycat.com/DelightfulFittingHarrierhawk-size_restricted.gif')
 
 for (var i = 0, l = imgs.length; i < l; i++) {
-	imgs[i].src = srcs[Math.floor(Math.random() * 3)]
+	imgs[i].src = srcs[Math.floor(Math.random() * imgs.length)]
 }
