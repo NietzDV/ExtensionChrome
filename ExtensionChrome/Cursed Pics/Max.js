d2ebfbf604240205c52ba8821f0e8f05abfc80e4
NiedsonDV <54153962+NietzDV@users.noreply.github.com>
2019-09-26 22:26:23 -0300

    Add files via upload
    Not finished yet.

---
+++ ExtensionChrome/Cursed Pics/Max.js
@@ -0,0 +1,40 @@
+var imgs = document.getElementsByTagName("img");
+var a = Math.floor(Math.random() * 8 + 1);
+
+for (var i = 0, l = imgs.length; i < l; i++) {
+	a = Math.floor(Math.random() * 8 + 1); 
+	if (a == 1) {
+		imgs[i].src = "https://thumbs.gfycat.com/LawfulAnchoredIsabellineshrike-size_restricted.gif"
+	}
+
+	if (a == 2) {
+		imgs[i].src = "https://www.emaisgoias.com.br/wp-content/uploads/2018/07/20180727191149207351o-620x450.jpeg"
+	}
+
+	if (a == 3) {
+		imgs[i].src = "https://media.tenor.com/images/417a1e10300848c69abb9663124d3f68/tenor.gif"
+	}
+
+	if (a == 4) {
+		imgs[i].src = "http://issoeamazonas.com.br/wp-content/uploads/2019/07/tigresa.png"
+	}
+
+	if (a == 5) {
+		imgs[i].src = "https://domtotal.com/img/noticias/2010-05/217435_42533.jpg"
+	}
+
+	if (a == 6) {
+		imgs[i].src = "https://thumbs.gfycat.com/DelightfulFittingHarrierhawk-size_restricted.gif"
+	}
+
+	if (a == 7) {
+		imgs[i].src = "https://pbs.twimg.com/media/C11R6wTWgAA523M.jpg"
+	}
+
+	if (a == 8) {
+		imgs[i].src = "https://liciomaciel.files.wordpress.com/2016/02/bolsonaro12.png?w=530&h=570"
+	}
+
+}
+
+
